7d8afa445a4d683e713775e7e58610b2c22e01b7
MD. Jahidul Islam Jony <jahiduljony60@gmail.com>
2024-11-27 22:22:54 +0600

    Add files via upload

---
+++ Distance.java
@@ -0,0 +1,17 @@
+//Problem - 1016;
+
+import java.util.Scanner;
+
+public class Distance
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int km = sc.nextInt();
+
+        int minutes = km * 2;
+
+        System.out.println("Minutos: " +minutes);
+
+    }
+}
