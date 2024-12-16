54bdb4b479841e388d0f8eff4271b8225b82b21f
MD. Jahidul Islam Jony <jahiduljony60@gmail.com>
2024-12-16 23:18:28 +0600

    Add files via upload

---
+++ Brazil_World_Cup.java
@@ -0,0 +1,24 @@
+//BeeCrowd Problem - 1564;
+
+import java.util.Scanner;
+
+public class Brazil_World_Cup
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            int N = sc.nextInt();
+
+            if (N == 0)
+            {
+                System.out.println("vai ter copa!");
+            }
+            else
+            {
+                System.out.println("vai ter duas!");
+            }
+        }
+    }
+}
