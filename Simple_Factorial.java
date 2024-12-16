aee80acea39acef08fe2787378f3a0380b69df3a
MD. Jahidul Islam Jony <jahiduljony60@gmail.com>
2024-12-16 23:08:25 +0600

    Add files via upload

---
+++ Simple_Factorial.java
@@ -0,0 +1,31 @@
+//BeeCrowd Problem - 1180;
+
+import java.util.Scanner;
+
+public class Simple_Factorial
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number (N): ");
+        int N = sc.nextInt();
+
+        if (N < 0)
+        {
+            System.out.println("Factorial is not defined for negative numbers.");
+        }
+
+        else
+        {
+            long factorial = 1;
+
+            for (int i = 1; i <= N; i++)
+            {
+                factorial *= i;
+            }
+
+            System.out.println("Factorial of " + N + " is: " + factorial);
+        }
+    }
+}
