fe811352845d28d2434b3311f09aa8b5df30aa1b
MD. Jahidul Islam Jony <jahiduljony60@gmail.com>
2024-12-16 23:34:28 +0600

    Add files via upload

---
+++ Electrical_Outlet.java
@@ -0,0 +1,20 @@
+//BeeCrowd Problem - 1930;
+
+import java.util.Scanner;
+
+public class Electrical_Outlet
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int T1 = sc.nextInt();
+        int T2 = sc.nextInt();
+        int T3 = sc.nextInt();
+        int T4 = sc.nextInt();
+
+        int maxDevices = (T1 - 1) + (T2 - 1) + (T3 - 1) + T4;
+
+        System.out.println(maxDevices);
+    }
+}
